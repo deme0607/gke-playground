92b79b5393769ecd1c6e4f953ee79d5687274dbf
Naoki Shimizu <naoki.shimizu@mercari.com>
2017-07-02 23:31:02 -0700

    enable api to respond request header

--- api/main.go
+++ api/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
+type Response struct {
+	Message       string      `json:"message"`
+	RequestHeader http.Header `json:"request_header"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "OK")
+		myres := Response{Message: "OK", RequestHeader: r.Header}
+		jsonStr, _ := json.Marshal(myres)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, string(jsonStr))
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", nil)
 }
